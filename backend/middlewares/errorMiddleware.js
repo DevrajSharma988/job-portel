b69ace774047330e564b501bfcacc8d61f87a4a5
Rahul Kumar <garatirahul11@google.com>
2026-08-27 23:24:15 +0530

    config & utils added

---
+++ backend/middlewares/errorMiddleware.js
@@ -0,0 +1,24 @@
+import STATUS_CODES from '../constants/statusCodes';
+
+const errorMiddleware = (err, req, res, next) => {
+  if (err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    return res.status(STATUS_CODES.CONFLICT).json({
+      success: false,
+      message: `${field} already exists`,
+    });
+  }
+
+  if (!err.statusCode || err.statusCode >= 500) {
+    console.error(err);
+  }
+
+  return res.status(err.statusCode || STATUS_CODES.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+    ...(err.code && { code: err.code }),
+    ...(err.data && { data: err.data }),
+  });
+};
+
+module.exports = errorMiddleware;
